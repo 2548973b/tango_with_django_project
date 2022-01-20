9b7cbe65d110f6bb1d667086f58020c830e259c5
Lorne Barnaby <2548973b@student.gla.ac.uk>
2022-01-20 22:34:51 +0000

    Chapter 4 DONE: Serving Media

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.urls import include
 from django.contrib import admin
 from django.urls import path
 from rango import views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('rango/', include('rango.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
